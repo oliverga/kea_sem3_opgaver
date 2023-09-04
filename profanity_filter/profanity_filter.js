857289a5900c96e9eb9da4b1060743d837635188
Oliver G.A <hi@oliverga.com>
2023-09-04 13:43:02 +0200

    theme switcher

--- profanity_filter/profanity_filter.js
+++ profanity_filter/profanity_filter.js
@@ -1,24 +1,39 @@
+"use strict";
+
 const curseWords = [
   {    
-    bad: "var",    
-    good: "const",  
+      bad: "var",    
+      good: "const",  
   },  
   {    
-    bad: "float",    
-    good: "grid",  
+      bad: "float",    
+      good: "grid",  
   },  
   {    
-    bad: "marquee",    
-    good: "just don't",  
+      bad: "marquee",    
+      good: "just don't",  
   },
 ];
 
 function profanityFilter(string) {  
-    let newString = string;
-    for (let i = 0; i < curseWords.length; i++) {
-        newString = newString.replace(curseWords[i].bad, curseWords[i].good);
-    }
-    return newString;
+  let newString = string;
+  let isUpdated = true;
+  for (let i = 0; i < curseWords.length; i++) {
+      if (newString.includes(curseWords[i].bad)) {
+          newString = newString.replace(curseWords[i].bad, `<span class="good-word">${curseWords[i].good}</span>`);
+          isUpdated = false;
+      }
+  }
+  if (isUpdated) {
+      let dialog = document.querySelector('#dialog');
+      dialog.showModal();
+      document.querySelector('#closeButton').addEventListener('click', function() {
+          dialog.close();
+      });
+  }
+  return newString;
 }
 
-console.log(profanityFilter("Programmering er sjovt! Men husk altid at bruge var, float og marquee i moderne kodning."));
+document.querySelector('#updateButton').addEventListener('click', function() {
+  document.querySelector('.text').innerHTML = profanityFilter(document.querySelector('.text').textContent);
+});
